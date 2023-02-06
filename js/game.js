fbac8ac8eab3fe0be234e33c7d200f1d699ffab9
Ihor Kaniuk <ikanyuk@icloud.com>
2023-02-06 19:58:03 +0200

    complete-task-cycles

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 const x = Math.round(Math.random() * (100  - 1) + 1);
-// console.log(x);
+console.log(x);
 let input; 
+let counter = 0;
 
 do {
     input = Number(prompt('Отгадай Х от 1 до 100'));
@@ -14,8 +15,11 @@ do {
     } else {
         alert(`Поздравляю Вы отгадали число! X = ${input}`);
     }
+    counter += 1;
+    
 } while (input !== x);
 
+console.log(`Число попыток ${counter}`);
 
 
 
